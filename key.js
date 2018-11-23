5cba7fb0d2ab02179c8984c9e654e7a0adb07c19
dev <tommykim1981@gmail.com>
2018-11-23 22:56:41 +0900

    ok

--- key.js
+++
@@ -1,9 +0,0 @@
-const Eos = require('eosjs');
-
-const ecc = Eos.modules.ecc;
-
-ecc.randomKey().then(privateKey => {
-    console.log('privateKey', privateKey);
-    const publicKey = ecc.privateToPublic(privateKey);
-    console.log("publicKey", publicKey);
-});
